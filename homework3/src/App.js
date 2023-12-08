9af4e1929f651cad265e96dee3c53f916252c999
Angel2001-programmer <angelwitchell2001@gmail.com>
2023-12-08 00:06:09 +0000

    Authentication working.

--- homework3/src/App.js
+++ homework3/src/App.js
@@ -3,9 +3,16 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './components/Login/Login';
 import FailedLogin from './components/FailedLogin/FailedLogin';
 import SuccessLogin from './components/SuccessLogin/SuccessLogin';
+import {useState, createContext, useEffect} from 'react';
+
+export const MContext = createContext();
 
 function App() {
+  const [isLogged, setisLogged] = useState(false);
+  console.log(isLogged);
+
   return (
+    <MContext.Provider value={[isLogged, setisLogged]}>
     <div className="App">
     <header className="App-header">
     <BrowserRouter>
@@ -17,6 +24,7 @@ function App() {
     </BrowserRouter>
     </header>
     </div>
+    </MContext.Provider>
   );
 }
 
--- homework3/src/components/Login/Login.jsx
+++ homework3/src/components/Login/Login.jsx
@@ -1,27 +1,57 @@
 import styles from "./Login.module.css";
 import Button from "../../UI/Button/button"
-import { Link } from "react-router-dom";
-import { useState } from "react";
-
+import { useState, useContext } from "react";
+import { MContext } from "../../App";
 const Login = props => {
-    const [userData, setUserData] = useState();
-    const userCredential = {email: "helloThere123@moogle.co.uk", password: "pass123"};
-    let button = <Button text="Login"/>;
+    const [errorMessage, setErrorMessage] = useState();
+    const [userData, setUserData] = useState({email: '', password: ''});
+    const [isLogged, setisLogged] = useContext(MContext);
 
+    const userCredential = {
+        email: "helloThere123@moogle.co.uk", 
+        password: "pass123"};
+
+    const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
     const onChangeInput = (e) => {
+        setUserData(null);
         setUserData({...userData, [e.target.name]: e.target.value});
-        console.log(userData);
+
+        if(userData.email === '' || userData.password === ''){
+            return setErrorMessage(<p className={styles.errorMessage}>Inputs cannot be empty.</p>);
+        } else {
+            setErrorMessage(null);
+        }
+        // Check if email address is valid.
+        if(!regex.test(userData.email)){
+            return setErrorMessage(<p className={styles.errorMessage}>Email or Password is not valid.</p>);
+        } else {
+            setErrorMessage(null);
+        }
     }
 
-    const checkUser = () => {
-        
+    const checkUser = (e) => {
+        e.preventDefault();
+        console.log(isLogged);
+        if (userCredential.email === userData.email &&
+            userCredential.password === userData.password){
+            setErrorMessage(null)
+            setisLogged(true);
+            console.log('Success!')
+            window.location.href = "/SuccessLogin";
+
+        } else {
+            setisLogged(false);
+            window.location.href = "/FailedLogin";
+
+        }
     }
 
     return(
         <div className={styles.container}>
-        <form className={styles.login} onSubmit={(e) => Login(e)}>
+        <form className={styles.login} onSubmit={(e) => checkUser(e)}>
             <h2>Login</h2>
             <div className={styles.inputCol}>
+            {errorMessage}
             <label className={styles.label} htmlFor="email">Enter Email</label>
             <div className={styles.inputRow}>
             <input className={styles.input} type="text" name="email" onChange={onChangeInput}/>
@@ -30,9 +60,8 @@ const Login = props => {
             <label className={styles.label} htmlFor="password">Enter Password</label>
             <input className={styles.input} type="password" name="password" onChange={onChangeInput}/></div>
             </div>
-            {/* <Link to="/SuccessLogin"><Button text="Login"/></Link> */}
-            {button}
-        </form>
+            <Button text="Login"/>            
+            </form>
         </div> 
     )
 }
